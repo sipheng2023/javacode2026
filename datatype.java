3ae13b99033300d89aca57ee68403bb79ccdfe45
sipheng2023 <123350069+sipheng2023@users.noreply.github.com>
2026-03-25 10:53:48 +0700

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,17 @@
+public class datatype {
+
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 3.14f;
+        double c = 3.14159;
+        char d = 'A';
+        boolean e = true;
+
+        System.out.println("Integer: " + a); // Output: Integer: 10
+        System.out.println("Float: " + b); // Output: Float: 3.14
+        System.out.println("Double: " + c); // Output: Double: 3.14159
+        System.out.println("Character: " + d); // Output: Character: A
+        System.out.println("Boolean: " + e); // Output: Boolean: true
+    }
+
+}
