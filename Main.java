350bb00e1fdb2c67f2d1d89a00bb2e3165d635cd
sipheng2023 <123350069+sipheng2023@users.noreply.github.com>
2026-03-25 10:31:51 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main  {
+
+    public static void main(String[] args){
+
+        // this is single comment line
+
+        System.out.println("ສະບາຍດີ");
+        System.out.print("Welcome to JAVA");
+        System.out.println(" For 2026 ");
+        System.out.println("Good jobs");
+
+        /*  this is multi comment line
+        this is
+        sample program for Java
+        first out put
+         in yar 2026 */
+
+    }
+
+}
